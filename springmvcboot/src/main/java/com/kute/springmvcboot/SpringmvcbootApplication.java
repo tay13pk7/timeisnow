92beeb443baaee9a4dfc6c23943f21c01930bdca
tay13pk7 <145957676+tay13pk7@users.noreply.github.com>
2023-10-04 00:13:17 +0530

    Add files via upload

---
+++ springmvcboot/src/main/java/com/kute/springmvcboot/SpringmvcbootApplication.java
@@ -0,0 +1,14 @@
+package com.kute.springmvcboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmvcbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmvcbootApplication.class, args);
+		System.out.println("hello");
+	}
+
+}
